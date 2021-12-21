6fafe9d368998771dcc78bfb821ff1ec61307fd0
Nikodimosendashaw <nikodimos1111endashaw@gmail.com>
2021-12-21 13:01:37 +0000

    3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 """
+this mmodule checks if the passed obj is a class or not
 """
 
 
 def is_same_class(obj, a_class):
-    """
+    """implementation
+    
+    args:
+        obj
+        a_class
+    return:
+        boolean
     """
     return isinstance(obj, a_class)
 ~                     
