e8e24cb5fc6e4b0755a4fd55cd1d8cc4440e21c1
Nikodimosendashaw <nikodimos1111endashaw@gmail.com>
2021-12-21 12:42:03 +0000

    2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,7 +6,6 @@
 def is_same_class(obj, a_class):
     """
     """
-    if isinstance(obj, a_class):
+    if type(obj) == a_class:
         return True
-    else:
-        return False
+    return False
