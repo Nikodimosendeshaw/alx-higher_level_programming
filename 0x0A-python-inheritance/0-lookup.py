c9a06780136de2038416cf788db4e1f14fd8c32b
Nikodimosendashaw <nikodimos1111endashaw@gmail.com>
2021-12-23 02:50:32 +0000

    100-my_int.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
+"""
+Returns the list of available attributes and methods of an object:
+"""
+
+
 def lookup(obj):
-    return(dir(obj))
+    """retrun list of attributes and methods of `obj`
+
+    Args:
+        obj (Any): object
+
+    Returns:
+        list: list of attributes and members
+    """
+    return dir(obj)
