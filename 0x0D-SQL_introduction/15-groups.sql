95d9085f2ab163da8bf9912c864741fa6978ccdf
Nikodimosendashaw <nikodimos1111endashaw@gmail.com>
2022-02-03 13:33:40 +0000

    15-groups.sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- 15-groups
-SELECT score, COUNT(score) as number FROM second_table GROUP BY score HAVING number
+SELECT score, COUNT(score) as number FROM second_table GROUP BY score HAVING number;
